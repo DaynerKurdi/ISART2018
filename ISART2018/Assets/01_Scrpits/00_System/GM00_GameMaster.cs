f0121841f2552339201f4a9c967a256a8b6953fc
DaynerKurdi <ziad_dayner@yahoo.com>
2018-08-26 21:30:24 +0900

    Added title and gamemanger

---
+++ ISART2018/Assets/01_Scrpits/00_System/GM00_GameMaster.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GM00_GameMaster : MonoBehaviour
+{
+    public static GM00_GameMaster gameMaster;
+
+    public delegate void SetupGameManager();
+
+    public SetupGameManager SETUP_GAMEMANGER_DELAEGATE;
+
+    private void Awake()
+    {
+        if (gameMaster != null || gameMaster != this)
+        {
+            Destroy(gameMaster);
+        }
+        else
+        {
+            gameMaster = this;
+
+            if (SETUP_GAMEMANGER_DELAEGATE != null)
+            {
+                SETUP_GAMEMANGER_DELAEGATE();
+            }
+        }
+    }
+}
